02ba5e2676493b6dd80f2bf1aa9d54460e5d32a0
Vasilii VV <b888bb63@gmail.com>
2025-06-03 14:48:23 +0400

    Обновление 0026

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from telegram.ext import (
 import threading
 import traceback
 
+# === Разрешённые пользователи ===
+ALLOWED_USERS = {527852428, 1411866927}
 
 # === Logging ===
 logging.basicConfig(
